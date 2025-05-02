b17ebece0f380089c47fe99b12902904a00b48bb
jcodeeee <janmer082003@gmail.com>
2025-05-02 17:46:10 +0800

    Update DashboardText.js

--- src/components/DashboardText.js
+++ src/components/DashboardText.js
@@ -3,7 +3,7 @@ const DashboardText = () => {
     return (
         <div>
             <h1 className="text-center text-2xl sm:text-4xl pt-5 text-white font-medium">
-                I MISS U HINDI MAPAKALI
+                MAGBASA KA NAMAN BOY!!
             </h1>
         </div>
     );
